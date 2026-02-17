bbae7102035f30111ef5e1fd86d5e9c28cfd6e14
bacs <brunobaacs@gmail.com>
2026-02-17 18:33:18 -0300

    parte 5 concluida

--- lib/credit_card_ledger/entity/user.ex
+++ lib/credit_card_ledger/entity/user.ex
@@ -19,4 +19,13 @@ defmodule CreditCardLedger.Entity.User do
     |> validate_required([:fullname]) # Garante que o nome não seja nulo
   end
 
+  # outra forma de fazer o changeset
+  # def cast(params) do
+  #   now = NaiveDateTime.truncate(NaiveDateTime.utc_now(), :second)
+  #   %__MODULE__{inserted_at: now}
+  #   |> cast(params, [:fullname]) # vai verificar se é string
+  #   |> validate_required([:fullname]) # nao pode ser nula, tem que testar aqui
+  #   |> apply_changes()
+  # end
+
 end
